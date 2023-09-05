466e9eda76e50c851423f04be23ba35a5363838a
a20140092 <131192869+a20140092@users.noreply.github.com>
2023-09-05 12:42:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("BIENVENIDO AL RESTAURANTE LA DELICIA");
+var entrada = parseInt(prompt("Ingrese el valor de la entrada"));
+var segundo = parseInt(prompt("Ingrese el valor del segundo"));
+var postre = parseInt(prompt("Ingrese el valor del postre"));
+alert("Precione F12 para ver su cuenta");
+// MOSTRAR LOS PRECIOS
+console.log("Entrada: "+entrada);
